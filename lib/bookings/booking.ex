607fb5580dec2193e125a3e6c5a696e395936f29
Augusto Hermes <augustohermes@outlook.com>
2023-03-11 19:56:04 -0300

    Desafio 05 - Elixir

---
+++ lib/bookings/booking.ex
@@ -0,0 +1,18 @@
+defmodule Flightex.Bookings.Booking do
+  @keys [:complete_date, :local_origin, :local_destination, :user_id, :id]
+
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build(complete_date, local_origin, local_destination, user_cpf) do
+    {:ok,
+     %__MODULE__{
+       complete_date: complete_date,
+       local_origin: local_origin,
+       local_destination: local_destination,
+       user_id: user_cpf,
+       id: UUID.uuid4()
+     }}
+  end
+end
